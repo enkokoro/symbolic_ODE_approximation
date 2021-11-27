22117cbf2e7cbb6b16ab8e5244928a5b1d49c33c
Evelyn Kuo <evelynkuoev@gmail.com>
2021-11-26 22:55:31 -0500

    initial commit

---
+++ approximation.py
@@ -0,0 +1,27 @@
+import sympy
+from numerical_methods import calculate_h, forward_euler
+
+def approximation(f, T, eps=1e-5, num_method=forward_euler, spline_deg=1):
+    """ 
+    symbolic function approximation to solution of
+    x'(t) = f(t, x) for 0 <= t <= T
+
+    inputs
+    f: RHS function of diff eq
+    T: maximum time
+    eps: global error
+    num_method: numerical method used for calculating discrete ODE approximation 
+    approx_method: method used for converting discrete approximation to continuous approximation
+
+    output
+    x_approx(x0, t): function which approximates solution to x'(t) = f(t, x) within eps
+    """
+    M = 1
+    h = 0.1 # calculate_h(M, T, eps, num_method)
+    _x0 = sympy.symbols("_x0")
+    ts, xs = num_method(f, _x0, T, h)
+
+    _t = sympy.symbols("_t")
+    f_approx = sympy.interpolating_spline(spline_deg, _t, ts, xs)
+
+    return sympy.lambdify([_x0, _t], f_approx)
---
+++ numerical_methods.py
@@ -0,0 +1,75 @@
+"""
+Numerical Methods
+
+numerical_method(f, x0, T, h)
+    calculates discrete approximation to the differential equation 
+    x'(t) = f(t, x) for 0 <= t <= T and initial value x0 using timestep h
+
+    input
+    f: function f representing differential equation x'(t) = f(t, x)
+    x0: initial value of x
+    T: maximum time
+    h: timestep
+
+    output
+    ts: discrete time values where we generated approximations
+    xs: approximations at the discrete time values
+
+numerical_method_h(M, T, eps)
+    calculates timestep necessary to obtain error of at most eps
+
+    input
+    M: maximum derivative of the function over the time range
+    T: maximum time
+    eps: desired error bound
+
+    output
+    h: timestep
+"""
+import numpy as np
+
+def calculate_h(M, T, eps, num_method):
+    calc_h_func = None
+    if num_method == forward_euler:
+        calc_h_func = forward_euler_h
+    
+    if calc_h_func is None:
+        raise NotImplementedError
+    else:
+        return calc_h_func(M, T, eps)
+
+
+def ODE_onestep(f, x0, T, h, onestep):
+    N = int(np.ceil(T/h)) 
+
+    xs = [x0]
+    ts = [0]
+
+    for i in range(N-1):
+        xs.append(onestep(f, ts[i], xs[i], h))
+        ts.append(ts[i] + h)
+
+    ts.append(T)
+    h_last = ts[-1] - ts[-2]
+    xs.append(xs[-2] + h_last*f(ts[-2], xs[-2]))
+
+    return ts, xs
+
+def forward_euler(f, x0, T, h):
+    def forward_euler_onestep(f, ti, xi, h):
+        return xi + h*f(ti, xi)
+
+    return ODE_onestep(f, x0, T, h, forward_euler_onestep)
+
+def forward_euler_h(M, T, eps):
+    # TODO
+    pass
+
+def runge_kutta_4(f, x0, T, h):
+    def rk4_onestep(f, ti, xi, h):
+        # TODO
+        pass
+
+def runge_kutta_4_h(M, T, eps):
+    # TODO
+    pass 
