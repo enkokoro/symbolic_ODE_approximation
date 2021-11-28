efe423a434513f33230a3a191c38ff0af3e26eb0
Evelyn Kuo <evelynkuoev@gmail.com>
2021-11-27 21:43:55 -0500

    update to account for vector functions

--- approximation.py
+++ approximation.py
@@ -1,13 +1,14 @@
 import sympy
 from numerical_methods import calculate_h, forward_euler
 
-def ODE_approx(f, M, L, T, eps=1e-5, num_method=forward_euler, spline_deg=1):
+def ODE_approx(f, dim, M, L, T, eps=1e-5, h=None, num_method=forward_euler, spline_deg=1):
     """ 
     symbolic function approximation to solution of
     x'(t) = f(t, x) for 0 <= t <= T
 
     inputs
-    f: RHS function of diff eq
+    f: RHS function of vector diff eq
+    dim: dimension of vector
     T: maximum time
     eps: global error
     num_method: numerical method used for calculating discrete ODE approximation 
@@ -16,11 +17,18 @@ def ODE_approx(f, M, L, T, eps=1e-5, num_method=forward_euler, spline_deg=1):
     output
     x_approx(x0, t): function which approximates solution to x'(t) = f(t, x) within eps
     """
-    h = calculate_h(M, L, T, eps, num_method)
-    _x0 = sympy.symbols("_x0")
+    if h is None:
+        h = calculate_h(M, L, T, eps, num_method)
+    _x0 = sympy.Matrix([sympy.symbols("_x"+str(i)) for i in range(1, dim+1)])
     ts, xs = num_method(f, _x0, T, h)
 
+    xis = [[] for i in range(dim)]
+    for x in xs:
+        for i in range(dim):
+            xis[i].append(x[i])
+
     _t = sympy.symbols("_t")
-    f_approx = sympy.interpolating_spline(spline_deg, _t, ts, xs)
+
+    f_approx = [sympy.interpolating_spline(spline_deg, _t, ts, xis[i]) for i in range(dim)]
 
     return sympy.lambdify([_x0, _t], f_approx)
